bd5f5d766c3de289623ebd9776992d66bf47c4ea
Marc MacLeod <marbemac@gmail.com>
2013-06-23 15:11:58 -0700

    initial commit
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PianoProject::Application.config.secret_key_base = 'c2742dc8ee0f3dd6c0f74f5b865d7135cc59e61097e4ad2fb69bf9a36c2944c34238c73305944b2716a78cc540dff379dd2198b16df88b9cd8cf36de43f5dfb0'
